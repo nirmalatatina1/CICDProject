b3056cf24e822eb90f603aea2ddfafb1aa3b1c7d
nirmalatatina1 <nirmalatatina1@gmail.com>
2024-05-09 15:39:07 +0530

    Terraform code added

---
+++ terraform_code/v1.ec2.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1" 
+    
+}
+
+resource "aws_instance" "demo-server"{
+    ami = "ami-04b70fa74e45c3917"
+    instance_type ="t2.micro"
+    key_name = "dpp"
+}
+
